59a55b78f6ab78e241af66ff1a2cda4d72d64c88
Ali Aqa Khaliqi <ali.khaliqi1993@gmail.com>
2025-06-19 15:46:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const input = document.getElementById("input")
+function reverseString(str){
+    return str.split("").reverse().join("")
+}
+function check(){
+    const value = input.value
+    const reverse = reverseString(value)
+    if(value === reverse){
+        alert("It is a Palindrome")
+    } else{
+        alert("No, It's not.")
+    }
+    input.value = ""
+
+}
